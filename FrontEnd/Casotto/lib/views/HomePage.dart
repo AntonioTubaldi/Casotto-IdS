ff08b8192ba91dee1247817cc93758c031f0a0d5
RaijinKensei <Itasolid95@gmail.com>
2022-08-28 20:12:20 +0200

    Upgrade HomePage
    Nuovo menu per la Homepage per accedere alle varie sezioni

--- FrontEnd/Casotto/lib/views/HomePage.dart
+++ FrontEnd/Casotto/lib/views/HomePage.dart
@@ -1,12 +1,11 @@
 import 'package:casotto/views/AllOmbrelloni.dart';
 import 'package:casotto/views/AllOrdini.dart';
 import 'package:casotto/views/Calendar.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'AllOmbrelloni.dart';
 import 'Menu.dart';
 import 'PaginaContatti.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -16,12 +15,79 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
+
           centerTitle: true,
           backgroundColor: Colors.teal,
           title: const Text('HomePage'),
         ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              const DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Colors.teal,
+                ),
+                child: Text(
+                  'Casotto',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 24,
+                  ),
+                ),
+              ),
+              /**
+               * cliccare l'icona per aprire la pagina;
+               * */
+              ListTile(
+                leading:  IconButton(icon: Icon(color:Colors.teal ,Icons.beach_access), onPressed: () {
+                  Navigator.pushNamed(
+                    context,
+                    AllOmbrelloniView.routeName,
+                  );
+                }),
+                title: Text('Visualizza Spiaggia'),
+              ),
+              ListTile(
+                leading: IconButton(icon: Icon(color:Colors.teal ,Icons.calendar_month), onPressed: () {
+                  Navigator.pushNamed(
+                    context,
+                    CalendarView.routeName,
+                  );
+                }),
+                title: const Text('Visualizza Prenotazioni'),
+              ),
+              ListTile(
+                leading: IconButton(icon: Icon(color:Colors.teal ,Icons.restaurant_menu), onPressed: () {
+                  Navigator.pushNamed(
+                    context,
+                    MenuView.routeName,
+                  );
+                }),
+                title: const Text('Visualizza Menù'),
+              ),
+              ListTile(
+                leading: IconButton(icon: Icon(color:Colors.teal ,Icons.menu_open_outlined), onPressed: () {
+                  Navigator.pushNamed(
+                    context,
+                    AllOrdiniView.routeName,
+                  );
+                }),
+                title: const Text('Visualizza Ordini'),
+              ),
+              ListTile(
+        leading: IconButton(icon: Icon(color:Colors.teal ,Icons.person), onPressed: () {
+          Navigator.pushNamed(
+            context,
+            PaginaContattiView.routeName,
+          );
+        }),
+        title: const Text('Contatti'),
+              ),],
+          ),
+        ),
         /**
-         * per visualizzare il background, andare su 
+         * per visualizzare il background, andare su
          * pubspec.yaml ->  assets:
                           - .env
                           - lib/images/
@@ -34,8 +100,8 @@ class HomePage extends StatelessWidget {
             image: DecorationImage(
                 image: AssetImage("lib/images/spiaggia.jpg"),
                 fit: BoxFit.cover),
-          ),
-          child: Center(
+          )));
+         /* child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
@@ -136,5 +202,6 @@ class HomePage extends StatelessWidget {
             ),
           ),
         ));
+  }*/
   }
 }
